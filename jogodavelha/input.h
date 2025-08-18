1cb48ce13d88e7f8ae0dd2544a2d5a845f1e46dc
Guilherme Martini <guilherme.dcmartini@gmail.com>
2025-08-18 16:12:36 -0300

    teste

---
+++ jogodavelha/input.h
@@ -0,0 +1,9 @@
+#ifndef INPUT_H
+
+#include <stdio.h>
+
+int input_tamanhoTabuleiro();
+void input_movimentoJogador(int *x, int *y);
+
+#endif
+
---
+++ jogodavelha/vitoria.h
@@ -0,0 +1,8 @@
+#ifndef VITORIA_H
+
+bool check_horizontal(char **tab);
+bool check_vertical(char **tab);
+bool check_diagonalPrimaria(char **tab);
+bool check_diagonalSecundaria(char **tab);
+
+#endif
