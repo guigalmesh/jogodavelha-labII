5dc089df0459ada4e15f51cc0fcfe30cae2a43b3
Guilherme Martini <guilherme.dcmartini@gmail.com>
2025-08-19 14:05:37 -0300

    logica do game loop

--- jogodavelha/logica.c
+++ jogodavelha/logica.c
@@ -1,8 +1,18 @@
 #include "logica.h"
+#include "vitoria.h"
 
-void game_loop(){
+void game_loop(char **tab){
     int gameOver = -1;
     while(gameOver = -1){
-        
+        jogada_circulo(tab);
+        gameOver = check_vitoria(tab);
+        if(gameOver == VITORIA_CIRCULO)
+            mensagem_vitoriaCirculo();
+            return;
+        jogada_xis(tab);
+        gameOver = check_vitoria(tab);
+        if(gameOver == VITORIA_X)
+            mensagem_vitoriaXis();
+            return;
     }
 }
